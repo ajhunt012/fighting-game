9a01a51b88b094350b6e61417c7f86d22922209c
Austin Hunt <austin.hunt.012@gmail.com>
2023-01-13 13:53:03 -0600

    started work on hitboxes

--- index.js
+++ index.js
@@ -5,21 +5,34 @@ canvas.width = 1056;
 canvas.height = 576;
 c.fillRect(0, 0, canvas.width, canvas.height);
 
-const gravity =0.2
+const gravity =0.7
 
 class Sprite {
-    constructor({position, velocity}) {
+    constructor({position, velocity, color = 'red'}) {
         this.position = position
         this.velocity = velocity
+        this.width = 50
         this.height = 150
         this.lastKey
+        this.attackBox = {
+            position: this.position ,
+            width: 100,
+            height: 50,
+        }
+        this.color = color
     }
 
 
     draw() {
-        c.fillStyle = "red"
-        c.fillRect(this.position.x, this.position.y, 50, this.height)
+        c.fillStyle = this.color
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+        //attack box fill//
+        c.fillStyle = 'green'
+        c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height
+        )
     }
+
     update(){
         this.draw()
         this.position.x += this.velocity.x
@@ -50,7 +63,8 @@ const enemy = new Sprite({
     velocity: {
         x:0,
         y:10
-    }
+    },
+    color: "blue"
 })
 
 
@@ -92,11 +106,15 @@ function animation(){
     }
 
     //enemies movements//
-
     if (keys.ArrowLeft.pressed && enemy.lastKey==='ArrowLeft') {
         enemy.velocity.x = -5
     } else if (keys.ArrowRight.pressed && enemy.lastKey==='ArrowRight') {
         enemy.velocity.x = 5
+    }
+
+    //detect collision//
+    if(player.attackBox.position.x + player.attackBox.width > enemy.position.x && player.attackBox.position.x <= enemy.position.x + enemy.width){
+      console.log("Hit")
     }
 }
 
@@ -113,7 +131,7 @@ window.addEventListener('keydown', (event) => {
             player.lastKey = 'a'
             break
         case 'w':
-            player.velocity.y = -10
+            player.velocity.y = -20
             break
 
         case 'ArrowRight':
@@ -125,7 +143,7 @@ window.addEventListener('keydown', (event) => {
            enemy.lastKey = 'ArrowLeft'
             break
         case 'ArrowUp':
-            enemy.velocity.y = -10
+            enemy.velocity.y = -20
             break
     }
     console.log(event.key)
